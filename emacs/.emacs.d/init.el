4a51bf226aa13b15461396f1ec35137755381e5c
MMz <mmz.source@gmail.com>
2017-10-21 13:39:44 +0200

    Add bbatsov solarized theme.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -61,6 +61,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(package-selected-packages (quote (smex use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
