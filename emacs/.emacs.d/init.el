d7246fd76c76fb2cb3bbb3530bab010aea81a372
MMz <mmzsource@users.noreply.github.com>
2020-11-19 22:18:53 +0100

    Add deft package and add debug messages

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -29,8 +29,24 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+(message "%s" "MMz: Delete old config.el")
+
+;; The function org-babel-load-file will create and load a new config.el
+;; only when the config.org file is newer than the config.el file.
+;; because I'm using dotfiles and creating a symlink to my config.org, the
+;; symlink won't be updated when I change my original config.org!
+;; Since I seldomly restart my emacs, this is the best solution for me:
+
+(defvar-local config-file (concat user-emacs-directory "config.el"))
+(when (file-exists-p config-file) (delete-file config-file))
+
+(message "%s" "MMz: Start loading config file.")
+
 ;;; Load the config to configure the rest of the packages
 (org-babel-load-file (concat user-emacs-directory "config.org"))
+
+(message "%s" "MMz: End loading config file.")
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -38,7 +54,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" "00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c" "c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" "7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" "0fffa9669425ff140ff2ae8568c7719705ef33b7a927a0ba7c5e2ffcfac09b75" "2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" "bbe6dc4337a5350a516d4041ef8dd4d019461dda41c45c83d7aff35e9ed127c1" "265beed2347158e958a085bd645d816bb7354942c35415cdaf0c0b58285e2967" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))
- '(package-selected-packages '(cider smex use-package)))
+ '(package-selected-packages '(deft cider smex use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
